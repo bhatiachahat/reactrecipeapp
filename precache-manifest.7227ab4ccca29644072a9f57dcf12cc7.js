1ae393f7fedbc0e9dd46699256dcce6e5524b5e8
bhatia100 <chahatbhatia2014@gmail.com>
2019-08-28 00:30:02 +0530

    Updates

---
+++ precache-manifest.7227ab4ccca29644072a9f57dcf12cc7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e763596ff172b91e4aa8726255a19886",
+    "url": "/reactrecipeapp/index.html"
+  },
+  {
+    "revision": "f5d23a99d5ca6a6a6a66",
+    "url": "/reactrecipeapp/static/css/2.2543d6d1.chunk.css"
+  },
+  {
+    "revision": "d24eb20f6578a23e59a4",
+    "url": "/reactrecipeapp/static/css/main.90609646.chunk.css"
+  },
+  {
+    "revision": "f5d23a99d5ca6a6a6a66",
+    "url": "/reactrecipeapp/static/js/2.8dadceea.chunk.js"
+  },
+  {
+    "revision": "d24eb20f6578a23e59a4",
+    "url": "/reactrecipeapp/static/js/main.ef3e1846.chunk.js"
+  },
+  {
+    "revision": "059fd76a595b03f1bf50",
+    "url": "/reactrecipeapp/static/js/runtime~main.1fc17c97.js"
+  }
+]);
